abbff4fe3876365215ad2f64468d96cbad057428
bhanu.shukla <shuklabhanuprakash@gmail.com>
2017-09-13 19:31:54 +0530

    product controller added.

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('BlurAdmin', [ 'ngAnimate', 'ui.bootstrap',
+var app =angular.module('BlurAdmin', [ 'ngAnimate', 'ui.bootstrap',
 		'ui.sortable', 'ui.router', 'ngTouch', 'toastr', 'smart-table',
 		"xeditable", 'ui.slimscroll', 'ngJsTree',
 		'angular-progress-button-styles',
