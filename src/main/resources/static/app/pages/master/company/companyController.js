a2b5c299b128ef60dc86ac81284ef2cfc4d2c1ae
bhanu_shukla <shuklabhanuprakash@gmail.com>
2017-09-17 16:02:50 +0530

    toaster n message in company

--- src/main/resources/static/app/pages/master/company/companyController.js
+++ src/main/resources/static/app/pages/master/company/companyController.js
@@ -1,6 +1,25 @@
-app.controller("CompanyController",function($scope,$http,$state){
+app.controller("CompanyController",function($scope,$http,$state,toastr,
+		toastrConfig){
 	
 	$scope.sellers=[];
+	var openedToasts = [];
+	$scope.options = {
+		autoDismiss : false,
+		positionClass : 'toast-top-center',
+		type : 'success',
+		timeOut : '5000',
+		extendedTimeOut : '2000',
+		allowHtml : false,
+		closeButton : false,
+		tapToDismiss : true,
+		progressBar : false,
+		newestOnTop : true,
+		maxOpened : 0,
+		preventDuplicates : false,
+		preventOpenDuplicates : false,
+		title : "",
+		msg : ""
+	};
 	
 	
 	$scope.$on('$stateChangeStart', function() {getSellers(); });
@@ -19,10 +38,18 @@ app.controller("CompanyController",function($scope,$http,$state){
 	$scope.deleteCompany=function(id){
 		$http.get("/gst/deleteSeller/"+id).then(function(response){
 			console.log(response)
+			const toasterTitle="Company";
+			const toasterMsg="deleted successfully";
+			const messageType="success";
+			$scope.openToast(toasterTitle,toasterMsg,messageType);
 			$scope.sellers=response.data;
 			
-		},function(error){
-			console.log("error",error);
+		}, function(error) {
+			console.log("error", error);
+			const toasterTitle="Failed to delete company";
+			const toasterMsg="please try again later.";
+			const messageType="error";
+			$scope.openToast(toasterTitle,toasterMsg,messageType);
 		});
 	}
 	
@@ -37,6 +64,22 @@ app.controller("CompanyController",function($scope,$http,$state){
 		getSellers();
 		$state.go("master.company.companyList");
 	}
+	
+	$scope.openToast = function (toasterTitle,toasterMsg,messageType) {
+		$scope.options.title=toasterTitle;
+		$scope.options.msg=toasterMsg;
+		$scope.options.type=messageType;
+	      angular.extend(toastrConfig, $scope.options);
+	      openedToasts.push(toastr[$scope.options.type]($scope.options.msg, $scope.options.title));
+	}
+	
+	$scope.clearToasts = function() {
+		toastr.clear();
+	};
+
+	$scope.$on('$destroy', function iVeBeenDismissed() {
+		angular.extend(toastrConfig, defaultConfig);
+	})
 	
 	
 	
--- src/main/resources/static/app/pages/master/company/companyRegistrationController.js
+++ src/main/resources/static/app/pages/master/company/companyRegistrationController.js
@@ -18,14 +18,26 @@ app.controller("CompanyRegistrationController",function($scope,$http,$stateParam
 	$scope.saveCompany=function(){
 		$http.post("/gst/seller",$scope.company).then(function(response){
 			console.log(response)
+			const toasterTitle="Company";
+			const toasterMsg="registered successfully";
+			const messageType="success";
+			$scope.openToast(toasterTitle,toasterMsg,messageType);
 			$scope.company=response.data;
-			$state.go("master.company.companyList");
+			$scope.goToCompanyList();
 			
 			
 		},function(error){
 			console.log("error",error);
+			const toasterTitle="Company registration failed";
+			const toasterMsg="please try again later.";
+			const messageType="error";
+			$scope.openToast(toasterTitle,toasterMsg,messageType);
 		});
 		
+	}
+	
+	$scope.goToCompanyList=function(){
+		$state.go("master.company.companyList");
 	}
 	
 });
